a34b489b959a8b3616e3e45da5098f48746deb05
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-09-08 13:13:52 -0300

    feat: create middleware to authenticate routes

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.middleware.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { UsersRepository } from '../../../../modules/users/infra/database/repositories/UsersRepositories';
+import { AppError } from '../errors/AppError';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const authHeader = req.headers.authorization;
+  const usersRepository = new UsersRepository();
+
+  if (!authHeader) {
+    throw new AppError('Invalid token');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      String(process.env.JWT_SECRET_KEY),
+    ) as IPayload;
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists', 401);
+    }
+
+    req.user = {
+      id: user_id,
+    };
+
+    next();
+  } catch (error) {
+    throw new AppError('Invalid token', 401);
+  }
+}
